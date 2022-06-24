b416ee14eca16b2508249876732688087040a939
Efoxa <105425051+Efoxa@users.noreply.github.com>
2022-06-24 09:25:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let fullName = "Efosa Usiosefe Ero";
+let height = "5feet 8inches";
+let country = "Nigeria";
+Bio = () => {
+  document.getElementById("myName").innerHTML = fullName;
+  document.getElementById("myHeight").innerHTML = height;
+  document.getElementById("myCountry").innerHTML = country;
+};
